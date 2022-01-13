4bbef34c99957f97a68caa08c8b01aeeaf56cb9b
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2022-01-13 14:47:00 +0100

    FEAT animated Sections

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75c5fdb0309646db"; }
+/******/ 		__webpack_require__.h = function() { return "8a200755de360475"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import {
   Image,
   useColorModeValue
 } from '@chakra-ui/react'
+import Section from '../components/section'
 const Page = () => {
   return (
     <Container>
@@ -42,6 +43,13 @@ const Page = () => {
           />
         </Box>
       </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Projects
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
