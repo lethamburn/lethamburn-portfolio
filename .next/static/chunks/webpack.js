1be828bd7a661081012d969002f4e5e074324827
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2022-01-20 11:37:23 +0100

    FEAT custom 404

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6972cbacb93ca2ec"; }
+/******/ 		__webpack_require__.h = function() { return "1e2ed135462dc0c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/404.js
@@ -0,0 +1,30 @@
+import NextLink from 'next/link'
+import Image from 'next/image'
+import {
+  Box,
+  Heading,
+  Text,
+  Container,
+  Divider,
+  Button
+} from '@chakra-ui/react'
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Heading as="h1" mt={4}>Not found</Heading>
+      <Text>These aren't the pages you're looking for...</Text>
+      <Divider my={6} />
+      <Image src="/images/contents/obi.jpg" width="600px" height="250" />
+      <Divider my={6} />
+
+      <Box my={6} align="center">
+        <NextLink href="/">
+          <Button colorScheme="gray">Return to home</Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
+}
+
+export default NotFound
