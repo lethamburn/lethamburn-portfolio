0d1e76a2a6c64fe4f43eb09a7bc4ef7dbf0a9a38
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2023-10-23 23:22:42 +0200

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -6,7 +6,8 @@ const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}  backgroundImage="url('/images/background.png')" backgroundRepeat="repeat">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+    <meta name="theme-color" content="#FF8906" />
     <meta name="description" content="Web developer passionate about science fiction, photography, comics, video games, death metal, and 3D printing. Visit my GitHub to see my work."/>
 
         <link rel="shortcut icon" href="/images/contents/avatar.png" />
